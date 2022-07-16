b2542e22a4c4b2f9fd5eca260004832b766e015e
red2fred2 <red2fred2base@live.com>
2022-07-16 13:15:38 -0500

    Switch from spaces to tabs

--- src/main.rs
+++ src/main.rs
@@ -28,18 +28,18 @@ static mut USB_MANAGER: Option<UsbManager> = None;
 #[allow(non_snake_case)]
 #[interrupt]
 unsafe fn USBCTRL_IRQ() {
-    match USB_MANAGER.as_mut() {
-        Some(manager) => manager.interrupt(),
-        None => (),
-    };
+	match USB_MANAGER.as_mut() {
+		Some(manager) => manager.interrupt(),
+		None => (),
+	};
 }
 
 #[panic_handler]
 fn panic(panic_info: &PanicInfo) -> ! {
-    if let Some(usb) = unsafe { USB_MANAGER.as_mut() } {
-        writeln!(usb, "{}", panic_info).ok();
-    }
-    loop {}
+	if let Some(usb) = unsafe { USB_MANAGER.as_mut() } {
+		writeln!(usb, "{}", panic_info).ok();
+	}
+	loop {}
 }
 
 // External high-speed crystal on the pico board is 12Mhz
@@ -47,54 +47,54 @@ pub const XOSC_CRYSTAL_FREQ: u32 = 12_000_000;
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
-    let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
-    let mut watchdog = Watchdog::new(pac.WATCHDOG);
-    let sio = Sio::new(pac.SIO);
+	let mut pac = pac::Peripherals::take().unwrap();
+	let core = pac::CorePeripherals::take().unwrap();
+	let mut watchdog = Watchdog::new(pac.WATCHDOG);
+	let sio = Sio::new(pac.SIO);
 
-    let clocks = hal::clocks::init_clocks_and_plls(
-        XOSC_CRYSTAL_FREQ,
-        pac.XOSC,
-        pac.CLOCKS,
-        pac.PLL_SYS,
-        pac.PLL_USB,
-        &mut pac.RESETS,
-        &mut watchdog,
-    )
-    .ok()
-    .unwrap();
+	let clocks = hal::clocks::init_clocks_and_plls(
+		XOSC_CRYSTAL_FREQ,
+		pac.XOSC,
+		pac.CLOCKS,
+		pac.PLL_SYS,
+		pac.PLL_USB,
+		&mut pac.RESETS,
+		&mut watchdog,
+	)
+	.ok()
+	.unwrap();
 
-    let usb = unsafe {
-        USB_BUS = Some(UsbBusAllocator::new(hal::usb::UsbBus::new(
-            pac.USBCTRL_REGS,
-            pac.USBCTRL_DPRAM,
-            clocks.usb_clock,
-            true,
-            &mut pac.RESETS,
-        )));
-        USB_MANAGER = Some(UsbManager::new(USB_BUS.as_ref().unwrap()));
-        // Enable the USB interrupt
-        pac::NVIC::unmask(hal::pac::Interrupt::USBCTRL_IRQ);
-        USB_MANAGER.as_mut().unwrap()
-    };
+	let usb = unsafe {
+		USB_BUS = Some(UsbBusAllocator::new(hal::usb::UsbBus::new(
+			pac.USBCTRL_REGS,
+			pac.USBCTRL_DPRAM,
+			clocks.usb_clock,
+			true,
+			&mut pac.RESETS,
+		)));
+		USB_MANAGER = Some(UsbManager::new(USB_BUS.as_ref().unwrap()));
+		// Enable the USB interrupt
+		pac::NVIC::unmask(hal::pac::Interrupt::USBCTRL_IRQ);
+		USB_MANAGER.as_mut().unwrap()
+	};
 
-    let pins = hal::gpio::Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut pac.RESETS,
-    );
+	let pins = hal::gpio::Pins::new(
+		pac.IO_BANK0,
+		pac.PADS_BANK0,
+		sio.gpio_bank0,
+		&mut pac.RESETS,
+	);
 
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
-    let mut led_pin = pins.gpio25.into_push_pull_output();
+	let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
+	let mut led_pin = pins.gpio25.into_push_pull_output();
 
-    loop {
-        led_pin.set_high().unwrap();
-        write!(usb, "On\n").unwrap();
-        delay.delay_ms(500);
+	loop {
+		led_pin.set_high().unwrap();
+		write!(usb, "On\n").unwrap();
+		delay.delay_ms(1000);
 
-        led_pin.set_low().unwrap();
-        write!(usb, "Off\n").unwrap();
-        delay.delay_ms(500);
-    }
+		led_pin.set_low().unwrap();
+		write!(usb, "Off\n").unwrap();
+		delay.delay_ms(1000);
+	}
 }
