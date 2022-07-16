5bf1901e6ff19d54987e25e2f7fd3c7f29813ce8
red2fred2 <red2fred2base@live.com>
2022-07-16 16:54:39 -0500

    Remove verbose flag for elf2uf2

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 cargo build --release
 
 # Use elf2uf2 to convert elf binary to uf2 that the RP2040 can understand
-./submodules/pico-sdk/tools/elf2uf2/elf2uf2 -v target/thumbv6m-none-eabi/release/blink target/thumbv6m-none-eabi/release/firmware.uf2
+./submodules/pico-sdk/tools/elf2uf2/elf2uf2 target/thumbv6m-none-eabi/release/blink target/thumbv6m-none-eabi/release/firmware.uf2
