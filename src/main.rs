d58c8614df2b52633d549556c0e679a4753e43ee
red2fred2 <red2fred2base@live.com>
2022-07-19 00:37:00 -0500

    Move code around to be in more coherent places This is just personal preference, with a few doc comments.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-//! Blinks the LED on a Pico board and log to USB console
-//!
-//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
-//! USB console can be used with write!() macro and is also used for panic! error message.
 #![no_std]
 #![no_main]
 
+mod usb;
+
 use core::fmt::Write as _;
 use core::panic::PanicInfo;
+
 use embedded_hal::digital::v2::OutputPin;
 use embedded_time::fixed_point::FixedPoint as _;
 use rp2040_hal as hal;
@@ -14,19 +13,22 @@ use rp2040_hal::{clocks::Clock as _, pac, pac::interrupt, sio::Sio, watchdog::Wa
 use usb_device;
 use usb_device::bus::UsbBusAllocator;
 
-mod usb_manager;
+use usb::usb_manager::UsbManager;
 
-use crate::usb_manager::UsbManager;
+/// External high-speed crystal on the pico board is 12Mhz
+pub const XOSC_CRYSTAL_FREQ: u32 = 12_000_000;
 
 #[link_section = ".boot2"]
 #[used]
+/// Boot loader
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 
 static mut USB_BUS: Option<UsbBusAllocator<hal::usb::UsbBus>> = None;
 static mut USB_MANAGER: Option<UsbManager> = None;
 
-#[allow(non_snake_case)]
 #[interrupt]
+/// USB Interrupt handler
+/// If USB_MANAGER is set, this shoots control over to its interrupt method
 unsafe fn USBCTRL_IRQ() {
 	match USB_MANAGER.as_mut() {
 		Some(manager) => manager.interrupt(),
@@ -35,42 +37,39 @@ unsafe fn USBCTRL_IRQ() {
 }
 
 #[panic_handler]
+/// For when the program shits its pants
 fn panic(panic_info: &PanicInfo) -> ! {
 	if let Some(usb) = unsafe { USB_MANAGER.as_mut() } {
 		writeln!(usb, "{}", panic_info).ok();
 	}
 	loop {}
 }
-
-// External high-speed crystal on the pico board is 12Mhz
-pub const XOSC_CRYSTAL_FREQ: u32 = 12_000_000;
-
 #[cortex_m_rt::entry]
 fn main() -> ! {
-	let mut pac = pac::Peripherals::take().unwrap();
+	// Set up hardware
+	// If any of these fail, the whole thing should shit the bed
+	let mut hardware = pac::Peripherals::take().unwrap();
 	let core = pac::CorePeripherals::take().unwrap();
-	let mut watchdog = Watchdog::new(pac.WATCHDOG);
-	let sio = Sio::new(pac.SIO);
+	let mut watchdog = Watchdog::new(hardware.WATCHDOG);
+	let sio = Sio::new(hardware.SIO);
 
 	let clocks = hal::clocks::init_clocks_and_plls(
 		XOSC_CRYSTAL_FREQ,
-		pac.XOSC,
-		pac.CLOCKS,
-		pac.PLL_SYS,
-		pac.PLL_USB,
-		&mut pac.RESETS,
-		&mut watchdog,
-	)
-	.ok()
-	.unwrap();
+		hardware.XOSC,
+		hardware.CLOCKS,
+		hardware.PLL_SYS,
+		hardware.PLL_USB,
+		&mut hardware.RESETS,
+		&mut watchdog
+	).ok().unwrap();
 
 	let usb = unsafe {
 		USB_BUS = Some(UsbBusAllocator::new(hal::usb::UsbBus::new(
-			pac.USBCTRL_REGS,
-			pac.USBCTRL_DPRAM,
+			hardware.USBCTRL_REGS,
+			hardware.USBCTRL_DPRAM,
 			clocks.usb_clock,
 			true,
-			&mut pac.RESETS,
+			&mut hardware.RESETS,
 		)));
 		USB_MANAGER = Some(UsbManager::new(USB_BUS.as_ref().unwrap()));
 		// Enable the USB interrupt
@@ -79,10 +78,10 @@ fn main() -> ! {
 	};
 
 	let pins = hal::gpio::Pins::new(
-		pac.IO_BANK0,
-		pac.PADS_BANK0,
+		hardware.IO_BANK0,
+		hardware.PADS_BANK0,
 		sio.gpio_bank0,
-		&mut pac.RESETS,
+		&mut hardware.RESETS,
 	);
 
 	let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
@@ -91,10 +90,10 @@ fn main() -> ! {
 	loop {
 		led_pin.set_high().unwrap();
 		write!(usb, "On\n").unwrap();
-		delay.delay_ms(1000);
+		delay.delay_ms(500);
 
 		led_pin.set_low().unwrap();
 		write!(usb, "Off\n").unwrap();
-		delay.delay_ms(1000);
+		delay.delay_ms(500);
 	}
 }
